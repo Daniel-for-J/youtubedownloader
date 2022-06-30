fd1629ad2977274e5d49b8ace4af0ce29a29acb5
jogunmade <jogundumade@gmail.com>
2022-06-30 01:36:42 +0100

    youtbe-downloader

---
+++ youtubedownloader.py
@@ -0,0 +1,20 @@
+from pytube import YouTube
+
+# Danny codes
+
+counter = input('How many videos do you want to download? ')
+
+
+for i in range(0, int(counter)):
+    result = None
+    while result == None:
+        try:
+          url = input("Type in url ")
+          video = YouTube(url)
+          videoDownloader = video.streams.get_highest_resolution()
+          print("Download In Progress...") 
+          videoDownloader.download()
+          print("Download successful")
+          break
+        except:
+          pass
